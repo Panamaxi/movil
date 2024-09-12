cef3c3718e30af74762fa39048126d6500e0ff6b
Panamaxi <maximilianoduarte824@gmail.com>
2024-09-12 01:44:37 -0300

    ultimos cambios y a.m

--- Aplicaciones moviles/RegistrAPP/src/app/app.module.ts
+++ Aplicaciones moviles/RegistrAPP/src/app/app.module.ts
@@ -5,10 +5,32 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Importaciones de Angular Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+// Importación de animaciones
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+
+    // Módulos de Angular Material
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    
+    // Proveedor de animaciones
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Aplicaciones moviles/RegistrAPP/src/app/pages/login/login.module.ts
+++ Aplicaciones moviles/RegistrAPP/src/app/pages/login/login.module.ts
@@ -1,17 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
 
+// Importaciones de Angular Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    
+    // Módulos de Angular Material
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
-  declarations: [LoginPage]
+  declarations: [LoginPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Esto es opcional, pero puede ayudar a evitar errores
 })
 export class LoginPageModule {}
